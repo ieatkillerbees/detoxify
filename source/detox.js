746610fb27896f1e6859086d9d822a51f28b8923
Samantha Quiñones <samantha.quinones@teamaol.com>
2015-01-06 23:38:17 -0500

    Initial commit

---
+++ source/detox.js
@@ -0,0 +1,44 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/GCrRdF
+	// who stole it from here: 
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\btoxin\b/g, "evil spirit");
+	v = v.replace(/\btoxins\b/g, "evil spirits");
+	v = v.replace(/\bToxin\b/g, "Evil Spirit");
+	v = v.replace(/\bToxins\b/g, "Evil Spirits");
+	v = v.replace(/\btoxic\b/g, "demonic");
+	v = v.replace(/\bToxic\b/g, "Demonic");
+	
+	textNode.nodeValue = v;
+}
